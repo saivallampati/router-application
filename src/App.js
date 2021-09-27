0c563f58daa5b63f06bca4b9ddae34000bb7c38c
Satyasai Vallampati <sai.vallampati@weinvest.net>
2021-09-27 11:28:02 +0530

    removed commented lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-// import Navbar from "./components/Navbar";
-// import Home from "./Pages/Home";
-// import SignIn from "./Pages/SignIn";
-// import globalContext from "./context/globalContext";
-// import About from "./Pages/About";
-// import Company from "./Pages/About/Containers/Company";
 import AppRouter from "./components/AppRouter";
 import "./App.css";
 
